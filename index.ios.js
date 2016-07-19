591dc9117555c97cdb28c1c0d435a7a07dfbb026
hozefaj <hozefarules@gmail.com>
2016-07-18 21:39:54 -0700

    Add start/stop logic + styles

--- index.ios.js
+++ index.ios.js
@@ -23,13 +23,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  time: {
+    fontSize: 60,
+  },
   buttonWrapper: {
     flex: 3,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
   },
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  startButton: {
+    borderColor: '#7fffd4',
+  },
+  stopButton: {
+    borderColor: '#dc143c',
+  },
+  lapButton: {
 
+  },
 });
 
 class Stopwatch extends Component {
@@ -37,16 +56,26 @@ class Stopwatch extends Component {
     super(props);
     this.state = {
       timeElasped: null,
+      timerRunning: false,
     };
     this.onStartPress = this.onStartPress.bind(this);
   }
 
   onStartPress() {
+    if (this.state.timerRunning) {
+      clearInterval(this.interval);
+      this.setState({
+        timerRunning: false,
+      });
+      return;
+    }
+
     const startTime = new Date();
 
-    setInterval(() => {
+    this.interval = setInterval(() => {
       this.setState({
         timeElasped: new Date() - startTime,
+        timerRunning: true,
       });
     }, 30);
   }
@@ -55,14 +84,17 @@ class Stopwatch extends Component {
     console.log('Lap was pressed');
   }
 
-  startButton() {
+  startStopButton() {
+    const style = this.state.timerRunning ? styles.stopButton : styles.startButton;
+
     return (
       <TouchableHighlight
         onPress={this.onStartPress}
         underlayColor="#f5f5dc"
+        style={[styles.button, style]}
       >
         <Text>
-          Start
+          {this.state.timerRunning ? 'Stop' : 'Start'}
         </Text>
       </TouchableHighlight>
     );
@@ -73,6 +105,7 @@ class Stopwatch extends Component {
       <TouchableHighlight
         onPress={this.onLapPress}
         underlayColor="#f5f5dc"
+        style={styles.button}
       >
         <Text>
           Lap
@@ -81,27 +114,21 @@ class Stopwatch extends Component {
     );
   }
 
-  border(color) {
-    return {
-      borderColor: color,
-      borderWidth: 4,
-    };
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <View style={[styles.header, this.border('#f0e68c')]}>
-          <View style={[this.border('#b22222'), styles.timerWrapper]}>
-            <Text>{formatTime(this.state.timeElasped)}</Text>
+        <View style={styles.header}>
+          <View style={styles.timerWrapper}>
+            <Text style={styles.time}>{formatTime(this.state.timeElasped)}</Text>
           </View>
-          <View style={[this.border('#8fbc8f'), styles.buttonWrapper]}>
-            {this.startButton()}
+
+          <View style={styles.buttonWrapper}>
+            {this.startStopButton()}
             {this.lapButton()}
           </View>
         </View>
 
-        <View style={[styles.footer, this.border('#009CDE')]}>
+        <View style={styles.footer}>
           <Text>
             I am list of laps
           </Text>
