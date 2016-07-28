0e8628b67df7e651a20c7f9ba93687e4f8c116e5
hozefaj <hozefarules@gmail.com>
2016-07-27 21:21:14 -0700

    Add CI & Gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+
+const ignoreList = [
+  '!**/node_modules/**/*',
+];
+
+const files = {
+  js: [
+    '**/src/*.js',
+    'index.ios.js',
+  ],
+};
+
+gulp.task('lint-js', () =>
+  gulp.src(files.js.concat(ignoreList))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
